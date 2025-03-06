6e211a453f23830427b7afd820c7a01d6fb4f03e
Cdotg <101446909@georgebrown.ca>
2025-03-06 19:47:50 +0000

    2 players added and stick

--- index.js
+++ index.js
@@ -23,13 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         update(){
             this.draw()
-            this.velocity.y += gravity
+
             this.position.y += this.velocity.y
         
 
             if(this.position.y + this.height + this.velocity.y >= canvas.height){
                 this.velocity.y = 0
-            }else this.velocity.y += gravity
+            } else this.velocity.y += gravity
+        
            
         }
 
